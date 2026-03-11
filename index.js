6b0b286dc4e52de695b4d6afc49a7b6efac17339
aadii-rawt <rawatadii060@gmail.com>
2026-03-11 20:49:50 +0530

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+app.get("/api/data", (req, res) => {
+  res.json({ message: "Hello from the API!" });
+});
+
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
+});
