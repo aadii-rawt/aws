43d378c5cd9f46e14efe35f785dd368f1efc0b60
aadii-rawt <rawatadii060@gmail.com>
2026-03-14 19:12:19 +0530

    action added fir dev

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
-
-const app = express();
-
+// this is production branch
+const app = express(); 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -14,3 +13,24 @@ app.get("/api/data", (req, res) => {
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
 });
+
+
+events {
+  # event 
+}
+
+http {
+    server {
+      listen 80;
+      server_name aws.dotdazzle.in;
+
+      location / {
+        proxy_pass http://localhost:8000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host $host;
+        proxy_cache_bypass $http_upgrade;
+      }
+    }
+}
