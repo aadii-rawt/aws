98229874e72de332bf48df23c6571960a4b3b566
aadii-rawt <rawatadii060@gmail.com>
2026-03-17 21:11:00 +0530

    docker file change

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
-// this is production branch
+
+// this is dev directory
 const app = express(); 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -14,23 +15,3 @@ app.listen(8000, () => {
   console.log("Server is running on port 8000");
 });
 
-
-events {
-  # event 
-}
-
-http {
-    server {
-      listen 80;
-      server_name aws.dotdazzle.in;
-
-      location / {
-        proxy_pass http://localhost:8000;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection 'upgrade';
-        proxy_set_header Host $host;
-        proxy_cache_bypass $http_upgrade;
-      }
-    }
-}
