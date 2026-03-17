517eb8de16c89d3cb782dc3328d89cd9cfd5a4b1
aadii-rawt <rawatadii060@gmail.com>
2026-03-17 21:16:50 +0530

    ssh key change

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.get("/api/data", (req, res) => {
   res.json({ message: "Hello from the API!" });
 });
 
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "docker file is running." });
+});
+
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
 });
